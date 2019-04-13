10942056c4efbdaf29dd10d75bf2600aec3506ff
Slava <slav@belaev.pro>
2019-04-13 22:32:26 +0300

    Updates

---
+++ precache-manifest.afe60c52d063584482677192f3ce4deb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/slavabelaev/react-todo-list/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a4466961a0c29d30dba4",
+    "url": "/slavabelaev/react-todo-list/static/js/runtime~main.3c910b23.js"
+  },
+  {
+    "revision": "419a04ee49764fcd023d",
+    "url": "/slavabelaev/react-todo-list/static/js/main.212d42f4.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/slavabelaev/react-todo-list/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "419a04ee49764fcd023d",
+    "url": "/slavabelaev/react-todo-list/static/css/main.fe1e147c.chunk.css"
+  },
+  {
+    "revision": "7df37ae1f0b1a33a5b150978880257a1",
+    "url": "/slavabelaev/react-todo-list/index.html"
+  }
+];
