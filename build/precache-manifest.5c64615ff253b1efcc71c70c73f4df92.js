f8fd1e9fdcb36ab1585507ff09a964d64eb8ac03
Slava <slav@belaev.pro>
2019-04-13 22:15:14 +0300

    build

---
+++ build/precache-manifest.5c64615ff253b1efcc71c70c73f4df92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "be7c76178872d8a05dc9",
+    "url": "/static/js/main.45fbde99.chunk.js"
+  },
+  {
+    "revision": "27b0ee05a0de71ffbd9a",
+    "url": "/static/js/2.2118023b.chunk.js"
+  },
+  {
+    "revision": "be7c76178872d8a05dc9",
+    "url": "/static/css/main.797f3c81.chunk.css"
+  },
+  {
+    "revision": "5ee707fdb0502f5163767263700ee57a",
+    "url": "/index.html"
+  }
+];
